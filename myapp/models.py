b9bac1ab0a4c44f665a893c6426adefc416b52ab
mbrsagor <mbrsagor@gmail.com>
2021-12-19 13:14:06 +0600

    New: Refactor project

--- myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Student(models.Model):
-    name = models.CharField(max_length = 30)
-    roll = models.IntegerField(default = 0)
-
-    def __str__(self):
-        return self.name
